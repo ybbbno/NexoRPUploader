c96471090e372bab5baa79b138969a2a6d02cb33
mitsu-wq <bmitsu779@gmail.com>
2026-09-06 13:30:28 +0700

    Initial commit

---
+++ src/main/java/com/sunphantom/nexorpuploader/NexoRPUploader.java
@@ -0,0 +1,17 @@
+package com.sunphantom.nexorpuploader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NexoRPUploader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/sunphantom/nexorpuploader/RPProvider.java
@@ -0,0 +1,4 @@
+package com.sunphantom.nexorpuploader;
+
+public class RPProvider {
+}
